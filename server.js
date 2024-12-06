681dadb7accf15258f812d14ed954dc023c93d71
OP (oppenheimer) <21008429+Ompragash@users.noreply.github.com>
2024-12-06 10:50:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Ansibler: test-argocd-image-updaterr');
+  res.send('Hello from Ansibler: v1');
 });
 
 app.listen(port, () => {
