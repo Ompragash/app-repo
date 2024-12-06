ea4a48fa6dc3a3f62869dae2407960048302d4b4
OP (oppenheimer) <21008429+Ompragash@users.noreply.github.com>
2024-12-06 11:29:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Ansibler: hi');
+  res.send('Hello from Ansibler: <3');
 });
 
 app.listen(port, () => {
